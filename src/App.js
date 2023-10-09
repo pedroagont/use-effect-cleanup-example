d379e78924f73f527ddb1878ac4047e4aa53c323
Pedro A. González <pgonzalez@post.com>
2023-10-09 11:21:37 -0600

    Add useEffect cleanup example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function ListItem(props) {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const counterInterval = setInterval(() => {
+      setCounter((prev) => prev + 1);
+      console.log(props.item);
+    }, 1000);
+    return () => clearInterval(counterInterval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>
+      {props.item}: {counter}
+      <button onClick={() => props.handleRemove(props.item)}>
+        remove timer
+      </button>
+    </li>
+  );
+}
+
+function App() {
+  const [items, setItems] = useState(['wow', 'awesome']);
+
+  const handleRemove = (item) => {
+    const newItems = items.filter((it) => it !== item);
+    setItems(newItems);
+  };
+
+  return (
+    <>
+      <h1>Aloo</h1>
+      <ul>
+        {items.map((item) => (
+          <ListItem key={item} item={item} handleRemove={handleRemove} />
+        ))}
+      </ul>
+    </>
   );
 }
 
